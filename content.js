f9734743301ab55242dc2ec3ba23f325718a59b3
Carlos Durán <carlosdurannet@gmail.com>
2025-07-12 18:45:26 +0200

    Errors fix

--- content.js
+++ content.js
@@ -1,10 +1,12 @@
-if (window.hasRun) {
-  return;
-}
-window.hasRun = true;
-
-browser.runtime.onMessage.addListener((message) => {
-  if (message.command === "invalidURL") {
-    alert(message.message);
+(() => {
+  if (window.hasRun) {
+    return;
   }
-});
+  window.hasRun = true;
+
+  browser.runtime.onMessage.addListener((message) => {
+    if (message.command === "invalidURL") {
+      alert(message.message);
+    }
+  });
+})();
